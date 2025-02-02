5abf2627363a6e6953a80d0cb9162b630bf2067c
FullFran <franfi17sw@gmail.com>
2025-02-02 14:25:59 +0100

    bug fix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://youtube2pdf-production.up.railway.app";  
+const API_BASE_URL = "https://youtube2pdf-production.up.railway.app/"; 
 
 async function getSubtitles() {
     const videoId = document.getElementById("videoId").value;
